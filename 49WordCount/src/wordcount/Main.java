20947933eaac1bef949933169e2f28acb64ec408
iamrajeevjain <117651441+iamrajeevjain@users.noreply.github.com>
2023-03-15 16:02:23 +0530

    Add files via upload

---
+++ 49WordCount/src/wordcount/Main.java
@@ -0,0 +1,14 @@
+package wordcount;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        CallMethods callMethods = new CallMethods();
+        callMethods.forStopWords();
+        callMethods.forNotStopWords();
+        
+             
+    }
+
+}
